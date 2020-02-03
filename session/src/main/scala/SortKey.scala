498ffdd99fcfa17fe62f10436f29c6474f539a7f
quwh <1422682170@qq.com>
2020-02-03 21:12:45 +0800

    自定义二次排序key

---
+++ session/src/main/scala/SortKey.scala
@@ -0,0 +1,12 @@
+//自定义 二次排序key
+case class SortKey(clickCount: Long, orderCount: Long, payCount: Long) extends Ordered[SortKey] {
+  override def compare(that: SortKey): Int = {
+    if (this.clickCount - that.clickCount != 0) {
+      return (this.clickCount - that.clickCount).toInt
+    } else if (this.orderCount - that.orderCount != 0) {
+      return (this.orderCount - that.orderCount).toInt
+    } else {
+      return (this.payCount - that.payCount).toInt
+    }
+  }
+}
