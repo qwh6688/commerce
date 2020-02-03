900a7721abfaf08d6a8811e978ca35f118b57a96
quwh <1422682170@qq.com>
2020-02-03 10:15:55 +0800

    0202

--- session/src/main/scala/SessionStat.scala
+++ session/src/main/scala/SessionStat.scala
@@ -1,4 +1,4 @@
-import java.util.{Date, UUID}
+import java.util.{Date, Random, UUID}
 
 import net.sf.json.JSONObject
 import commons.conf.ConfigurationManager
@@ -10,6 +10,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object SessionStat {
 
@@ -32,16 +33,112 @@ object SessionStat {
     //    sessionId2FullInfoRDD.foreach(println) 累加器中维护的是 Map结构的数据
     val sessionStatisticAccumulator = new SessionAccumulator
     sparkSession.sparkContext.register(sessionStatisticAccumulator)
-    //
+
+    // sessionId2FilterRDD: RDD[(sessionId, fullInfo)] 是所有符合过滤条件的数据组成的RDD
+    // getSessionFilteredRDD: 实现根据限制条件对session数据进行过滤，并完成累加器的更新
     val sessionId2FilterRDD = getSessionFilteredRDD(taskParam, sessionId2FullInfoRDD, sessionStatisticAccumulator)
     /*
     不加上这一句相当与没有action算子，数据库中的数据不准确
      */
-    sessionId2FilterRDD.foreach(println(_))
+    sessionId2FilterRDD.count()
     getSessionRatio(sparkSession, taskUUID, sessionStatisticAccumulator.value)
+
+    // 需求二：session随机抽取   数量问题，随机问题
+    // sessionId2FilterRDD： RDD[(sid, fullInfo)] 一个session对应一条数据，也就是一个fullInfo
+    sessionRandomExtract(sparkSession, taskUUID, sessionId2FilterRDD)
+    //    randomExtract.foreach(println)
 
 
   }
+
+
+  def generateRandomIndexList(extractPerDay: Int, daySessionCount: Long, hourCountMap: mutable.HashMap[String, Long], hourListMap: mutable.HashMap[String, ListBuffer[Int]]) = {
+    for ((hour, count) <- hourCountMap) {
+      var hourExtractCount = (count / daySessionCount.toDouble) * extractPerDay.toInt
+      // 避免一个小时要抽取的数量超过这个小时的总数 校验
+      if (hourExtractCount > count) {
+        hourExtractCount = count
+      }
+      val random = new Random()
+
+      hourListMap.get(hour) match {
+        case None => hourListMap(hour) = new ListBuffer[Int]
+          for ( i <- 0 until hourExtractCount){
+            var index = random.nextInt(count.toInt)
+            while (hourListMap(hour).contains(index)){
+              index = random.nextInt(count.toInt)
+            }
+            hourListMap(hour).append(index)
+          }
+        case Some(list) =>{
+          for ( i <- 0 until hourExtractCount){
+            var index = random.nextInt(count.toInt)
+            while (hourListMap(hour).contains(index)){
+              index = random.nextInt(count.toInt)
+            }
+            hourListMap(hour).append(index)
+          }
+        }
+      }
+
+
+
+
+    }
+  }
+
+  def sessionRandomExtract(sparkSession: SparkSession, taskUUID: String, sessionId2FilterRDD: RDD[(String, String)]) = {
+    // dateHour2FullInfoRDD: RDD[(dateHour, fullInfo)]
+    val dateHour2FullInfoRDD: RDD[(String, String)] = sessionId2FilterRDD.map {
+      case (sid, fullInfo) => {
+        val startTime = StringUtils.getFieldFromConcatString(fullInfo, "\\|", Constants.FIELD_START_TIME)
+        val dateHour: String = DateUtils.getDateHour(startTime)
+        (dateHour, fullInfo)
+      }
+    }
+    val hourCountMap: collection.Map[String, Long] = dateHour2FullInfoRDD.countByKey()
+
+    val dateHourCountMap = new mutable.HashMap[String, mutable.HashMap[String, Long]]()
+    for ((dateHour, count) <- hourCountMap) {
+      val date: String = dateHour.split("_")(0)
+      val hour = dateHour.split("_")(1)
+      //scala会自动解析类型
+      dateHourCountMap.get(date) match {
+        case None => dateHourCountMap(date) = new mutable.HashMap[String, Long]()
+          dateHourCountMap(date) += (hour -> count)
+        case Some(map) => dateHourCountMap(date) += (hour -> count)
+
+      }
+    }
+    // 经过这一步，dateHourCountMap 已有数据 数据结构[dateHour,hashMap[hour,count]],每小时的session数量；
+
+    // 解决问题一： 一共有多少天： dateHourCountMap.size
+    //              一天抽取多少条：100 / dateHourCountMap.size
+    val extractPerDay = 100 / dateHourCountMap.size
+
+    // 解决问题二： 一天有多少session：dateHourCountMap(date).values.sum
+    // 解决问题三： 一个小时有多少session：dateHourCountMap(date)(hour)
+
+    val dateHourExtractIndexListMap = new mutable.HashMap[String, mutable.HashMap[String, ListBuffer[Int]]]()
+    for ((date, hourCountMap) <- dateHourCountMap) {
+      val dateSessionCount = hourCountMap.values.sum
+
+      dateHourExtractIndexListMap.get(date) match {
+        case None => dateHourExtractIndexListMap(date) = new mutable.HashMap[String, ListBuffer[Int]]()
+          generateRandomIndexList(extractPerDay, dateSessionCount, hourCountMap, dateHourExtractIndexListMap(date))
+        case Some(map) =>
+          generateRandomIndexList(extractPerDay, dateSessionCount, hourCountMap, dateHourExtractIndexListMap(date))
+      }
+      // 到目前为止，我们获得了每个小时要抽取的session的index
+
+      // 广播大变量，提升任务性能
+      val dataHourExtractIndexListMapBd = sparkSession.sparkContext.broadcast(dateHourExtractIndexListMap)
+
+    }
+
+
+  }
+
 
   def getSessionRatio(sparkSession: SparkSession, taskUUID: String, value: mutable.HashMap[String, Int]): Unit = {
     val session_count = value.getOrElse(Constants.SESSION_COUNT, 1).toDouble
@@ -160,6 +257,7 @@ object SessionStat {
       (if (categoryIds != null) Constants.PARAM_CATEGORY_IDS + "=" + categoryIds else "")
     if (filterInfo.endsWith("\\|"))
       filterInfo = filterInfo.substring(0, filterInfo.length - 1)
+    // val result: RDD[(String, String)] =
     sessionId2FullInfoRDD.filter {
       case (sessionId, fullInfo) => {
         var success = true
@@ -176,21 +274,18 @@ object SessionStat {
         } else if (!ValidUtils.in(fullInfo, Constants.FIELD_CLICK_CATEGORY_IDS, filterInfo, Constants.PARAM_CATEGORY_IDS)) {
           success = false
         }
+        //更新累加器数据
         if (success) {
           sessionAccumulator.add(Constants.SESSION_COUNT)
-
           val visitLength = StringUtils.getFieldFromConcatString(fullInfo, "\\|", Constants.FIELD_VISIT_LENGTH).toLong
           val stepLength = StringUtils.getFieldFromConcatString(fullInfo, "\\|", Constants.FIELD_STEP_LENGTH).toLong
-
           calculateVisitLength(visitLength, sessionAccumulator)
           calculateStepLength(stepLength, sessionAccumulator)
         }
 
         success
       }
-
     }
-
 
   }
 
